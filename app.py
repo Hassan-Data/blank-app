59fe3216afb923bb4758f818158cf56708d59b5e
Hassan-Data <hassan_mohamed.abouelfetouh_noureldin@euruni.edu>
2025-03-10 01:11:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ if not os.path.exists(model_path):
 
 # Load the model safely
 try:
-    with open("diabetes_rf_model_fixed.pkl", "rb") as f:
+    with open("diabetes_rf_model_fixedn.pkl", "rb") as f:
         model = pickle.load(f)
 
     if not hasattr(model, "predict"):
